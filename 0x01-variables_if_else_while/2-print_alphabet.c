1499b3386c9dea5ebf3639e9488e1bc342e92149
Nomfutheko <thekothandiwe@gmail.com>
2022-02-04 14:32:40 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,11 +6,12 @@
  */
 int main(void)
 {
+
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-		patchar(c);
-		patchar('\n');
+		putchar(c);
+		putchar('\n');
 
-		return (0);
+return (0);
 }
