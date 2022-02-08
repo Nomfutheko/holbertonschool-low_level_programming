db0eb1cec8238a0c23d176ccff5953273fe70238
Nomfutheko <thekothandiwe@gmail.com>
2022-02-08 15:46:46 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,25 +9,13 @@
 int main(void)
 {
 	int i;
-<<<<<<< HEAD
+	
 	char p[] ="_putchar";
 
 for (i = 0; i < 8; i++)
 {
 _putchar(p[i]);
 }
-
-
 _putchar('\n');
 return (0);
-=======
-	char p[] = "_putchar";
->>>>>>> a37604463856ac6f300b35d186807254c0c7f489
-
-	for (i = 0; i < 8; i++)
-		{
-		_putchar(p[i]);
-		}
-		_putchar('\n');
-	return (0);
 }
