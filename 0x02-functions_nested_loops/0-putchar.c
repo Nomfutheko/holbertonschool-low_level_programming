89dbd2632c65b4b143851ec1ea47c3231718023b
Nomfutheko <thekothandiwe@gmail.com>
2022-02-08 12:45:11 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
 * main - Print the word "Holberton"
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int i;
-	char p[] = "Holberton";
+	char p[] = _putchar";
 
 	for (i = 0; i < 9; i++)
 		{
