6c5fa544870794ed82d9f84e73c49b448ae932c3
Nomfutheko <thekothandiwe@gmail.com>
2022-02-08 14:47:30 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,17 @@
  * return: 1 is greater than zero. 0 is zero.
  * -1 is less than zero.
  */
-int print_sign(int n )
+int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if (n < 0 )
+	else if (n < 0)
 	{
 		_putchar(45);
-		return(-1);
+		return (-1);
 	}
 	else
 	{
