02f7680843939e0f4590f849e4cec071a18a1b55
Nomfutheko <thekothandiwe@gmail.com>
2022-02-10 12:25:27 +0200

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,9 @@ void more_numbers(void)
 	int i;
 	char c;
 
-	for  (i = 0; i <10; i++)
+	for  (i = 0; i < 10; i++)
 	{
-		for (c = 0; c<= 14; c++)
+		for (c = 0; c <= 14; c++)
 		{
 			if (c < 9)
 			_putchar((c / 10) + '0');
