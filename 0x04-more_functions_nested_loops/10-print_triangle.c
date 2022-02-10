acb161690ea2a5ec4b71193afa57a706d6d7c30e
Nomfutheko <thekothandiwe@gmail.com>
2022-02-10 12:36:10 +0200

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,16 +1,15 @@
 #include "main.h"
-
 /**
- *print_triangle - prints a triangle
- @size: take in a integer for size of traingle
- */
+* print_triangle - prints a triangle
+* @size: takes in a integer for size of triangle
+*/
 void print_triangle(int size)
 {
-	int roe, column, k;
+	int row, column, k;
 
 	if (size <= 0)
 		_putchar('\n');
-	for (row - 0; row < size; row++)
+	for (row = 0; row < size; row++)
 	{
 		for (column = size - row; column > 1; column--)
 			_putchar(' ');
