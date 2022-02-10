312cc7d8c2d427e1c5f37d7ea25c7e8c61735ea2
Nomfutheko <thekothandiwe@gmail.com>
2022-02-10 12:24:29 +0200

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,9 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * main -  prints  the numbers from 0 to 9.
- */
-void print_numbers(void)
+* print_most_numbers -  prints the numbers except 2 and 4.
+*/
+void print_most_numbers(void)
 {
 	char c;
 
