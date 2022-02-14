bc2e6738a08a6b42df54935719a7b9fe262fe41f
Nomfutheko <thekothandiwe@gmail.com>
2022-02-14 13:55:19 +0200

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 #include "main.h"
-
 /**
- * _strcpy - update value.
- * @dest: value to be evaluated.
- * @src: value to be evaluated.
- * Return: nothing.
+ *_strcpy - update value.
+ *@dest: value to be evaluate.
+ *@src: value to be evaluate.
+ *Return: not.
  */
 char *_strcpy(char *dest, char *src)
 {
 	int l = 0;
 	int x = 0;
 
-	while (*(src + 1) != '\0')
+	while (*(src + l) != '\0')
 	{
 		l++;
 	}
