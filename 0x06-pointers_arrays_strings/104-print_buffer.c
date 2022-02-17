1367f382975008ccffc51f24c49421d791c93303
Nomfutheko <thekothandiwe@gmail.com>
2022-02-17 13:58:03 +0200

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,48 +2,60 @@
 #include <stdio.h>
 
 /**
- *print_buffer - Print a buffer 10 bytes per line.
- *@b: Buffer address.
- *@size: Number of characters to be printed.
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
+ * Return: void
+ */
+
+void print_line(char *c, int s, int l)
+{
+	int j, k;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
+	}
+}
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
  */
 void print_buffer(char *b, int size)
 {
-	int i, pi, c;
+	int i;
 
-	if (size <= 0)
-		printf("\n");
-	else
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
-		for (i = 9, pi = 0; i < size; i += 10)
-		{	printf("%.8x: ", pi);
-				for (c = pi; c <= i ; c++)
-				{	printf("%.2x", b[c]);
-					if (c % 2 == 1)
-						printf(" ");	}
-				for (c = pi; c <= i ; c++)
-				{
-					if (b[c] < 32)
-						printf(".");
-					else
-						printf("%c", b[c]);	}
-					printf("\n");
-					pi = i + 1;	}
-		if (pi < size - 1)
-		{	i -= 9;
-			printf("%.8x: ", i);
-			for (c = i; c < i + 10; c++)
-			{
-				if (c < size)
-					printf("%.2x", b[c]);
-				else
-					printf("  ");
-				if (c % 2 == 1)
-					printf(" ");	}
-			for (; i < size ; i++)
-			{
-				if (b[i] < 32)
-					printf(".");
-				else
-					printf("%c", b[i]);
-			}
-			printf("\n");	}	}	}
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		putchar('\n');
+	}
+	if (size == 0)
+		putchar('\n');
+}
