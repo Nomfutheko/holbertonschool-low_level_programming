959d007dce58dcadec9fd3640970daf73ccbaf4b
Nomfutheko <thekothandiwe@gmail.com>
2022-02-21 14:02:12 +0200

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _strchr - locate character in sring
- * @s: source string
- * @c: character to find
- *
- * Return: the string from character found
- */
-char *_strch(char *s, char c)
+  * _strchr - locate character in string
+  * @s: source string
+  * @c: character to find
+  *
+  * Return: the string from character found
+  */
+char *_strchr(char *s, char c)
 {
 	int a = 0, b;
 
@@ -15,13 +15,15 @@ char *_strch(char *s, char c)
 	{
 		a++;
 	}
-	for (b = 0; b <=a; b++)
+
+	for (b = 0; b <= a; b++)
 	{
 		if (c == s[b])
 		{
 			s += b;
-	return(s);
+			return (s);
 		}
 	}
+
 	return ('\0');
 }
