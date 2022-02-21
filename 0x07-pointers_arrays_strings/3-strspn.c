ee7c7b31842900fe7b8e9cf2542e1129b61634dc
Nomfutheko <thekothandiwe@gmail.com>
2022-02-21 03:52:05 -0800

    pointers,arrays and strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,4 @@
+#include "main.h"
+
+/**
+ * _strspn - search a sr
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, charc);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char * strstr(char *haystack, char *needle);
+void print chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
+void print_diagsums(int *a, int size);
