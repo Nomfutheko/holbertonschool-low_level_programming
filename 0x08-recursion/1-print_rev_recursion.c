2a4043a4ea7d2540931fe54ffd76ad3724345e83
Nomfutheko <thekothandiwe@gmail.com>
2022-02-23 12:16:52 +0200

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
-_print_rev_recussion - print a string in reverse
-@s: the string to reverse
-
-Return : nothing
-*/
-void _Print_rev_recusion(char *s)
+  * _print_rev_recursion - Print a string in reverse
+  * @s: the string to reverse
+  *
+  * Return: Nothing.
+  */
+void _print_rev_recursion(char *s)
 {
-if (*s == '\0')
-{
-return;
-}
+	if (*s == '\0')
+	{
+		return;
+	}
 
-s++;
-_print_rev_recusion(s);
-s--;
-_putchar(*s);
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
