e49fec194eaf778720142ba5e0da28e60c695571
Nomfutheko <thekothandiwe@gmail.com>
2022-02-23 12:23:59 +0200

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -15,38 +15,4 @@ int _strlen__recursion(char *s)
 
 	s++;
 	return (_strlen_recursion(s) + 1);
-}#include "main.h"
-
-/**
- * _strlen_recusion - Calculates the length of a string
- * @s: the string to count
- *
- * Return: integer value
- */
-int _strlen__recursion(char *s)
-{
-	if (*s == '\0')
-	{
-		return(0);
-	}
-
-	s++;
-	return (_strlen_recursion(s) + 1);
-}#include "main.h"
-
-/**
- * _strlen_recusion - Calculates the length of a string
- * @s: the string to count
- *
- * Return: integer value
- */
-int _strlen__recursion(char *s)
-{
-	if (*s == '\0')
-	{
-		return(0);
-	}
-
-	s++;
-	return (_strlen_recursion(s) + 1);
 }
