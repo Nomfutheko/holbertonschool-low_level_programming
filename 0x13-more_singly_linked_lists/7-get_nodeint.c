294a7570f9df5929bfd520bb0ccab5cefca4b8e3
Nomfutheko <thekothandiwe@gmail.com>
2022-03-22 12:18:59 +0200

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,12 +7,11 @@
  *
  * Return: pointer to the indexed node
  */
-
-listint_t *get_nodeint_at_index(listint_t *head, unsugned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head == Null)
+	if (head == NULL)
 		return (NULL);
 	for (i = 0; i < index; i++)
 	{
