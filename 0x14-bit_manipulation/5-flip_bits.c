de1bcb0917418434dda29ab0dfe605e0fe410c4e
Nomfutheko <thekothandiwe@gmail.com>
2022-03-24 12:37:47 +0200

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,17 +8,15 @@
  * Return: The number of bits you would need to flip to get from one number to
  * another.
  */
-
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsugned long int i = 0;
-	unsigned long int bits  = 0;
+	unsigned long int i = 0;
+	unsigned long int bits = 0;
 
 	while (i < 64)
 	{
-		if ((((m ^ n) >> i) & 1) ==1)
+		if ((((m ^ n) >> i) & 1) == 1)
 			bits = bits + 1;
-
 		i++;
 	}
 	return (bits);
